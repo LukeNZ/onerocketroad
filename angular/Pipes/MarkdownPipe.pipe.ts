6a48ca13f45d436cc28fe6355e0ec7909c4c1336
Luke <luke@Lukes-iMac.local>
2016-07-20 00:01:15 +1200

    tweet markdown support

--- angular/Pipes/MarkdownPipe.pipe.ts
+++ angular/Pipes/MarkdownPipe.pipe.ts
@@ -43,77 +43,112 @@ export class MarkdownPipe implements PipeTransform {
         return null;
     }
 
+    /**
+     * A function that provides rich text image viewing that matches a syntax of:
+     * ![caption](image-link "Attribution" left|right|wide), rendering it into an image
+     * with
+     *
+     * @param md    The instance of Remarkable.
+     */
     private parseRichImages(md) : void {
 
     }
 
     /**
-     * A function that provides an inline video parser that matches a syntax of:
-     * @[](video-link), rendering it into an iframe of a YouTube video embed.
+     * A function that provides an inline YouTube video player that matches a syntax of:
+     * @[caption](video-link), rendering it into an iframe of a YouTube video embed.
      *
      * @param md    The instance of Remarkable.
      */
     private parseVideos(md) : void {
         md.inline.ruler.push('video', state => {
-
-            // Store local copy of position so we don't overwrite it if we need to revert.
-            let pos = state.pos;
-            let marker = state.src.charCodeAt(pos++);
-
-            // Expect a series of tokens that matches the substring "@[](". This defines
-            // the start of a video.
-            if (marker !== 0x40 /* @ */) { return false; }
-            marker = state.src.charCodeAt(pos++);
-            if (marker !== 0x5B /* [ */) { return false; }
-            marker = state.src.charCodeAt(pos++);
-            if (marker !== 0x5D /* ] */) { return false; }
-            marker = state.src.charCodeAt(pos++);
-            if (marker !== 0x28 /* ( */) { return false; }
-
-            // Begin URL. While the current position is less than the length of the string,
-            // increment until we find a closing tag ")". Everything in between is the URL.
-            let urlStart = pos;
-            while (pos < state.posMax) {
-                marker = state.src.charCodeAt(pos++);
-
-                if (marker === 0x29 /* ) */) {
-                    break;
-                }
-            }
-
-            // Generate our token
-            let token = {
-                type: "video",
-                level: state.level,
-                src: state.src.slice(urlStart, pos - 1),
-                content: marker
-            };
-
-            // Update the state position as the token successfully completed, push the token
-            // onto the tokens array, and return true.
-            state.pos = pos;
-            state.push(token);
-            return true;
+            return this.parseMedia(state, '@', 'video');
         });
 
         // Render our video in an iframe.
         md.renderer.rules.video = (tokens, idx, options) => {
-            let src = tokens[idx].src;
-            return '<iframe width="1280" height="720" src="' + src
+            let link = tokens[idx].link;
+            return '<iframe width="1280" height="720" src="' + link
                 +'?rel=0" frameborder="0" allowfullscreen></iframe>';
         };
     }
 
+    /**
+     * A function that provides a block level Tweet display that matches a syntax of:
+     * #[caption](tweet-link), rendering it into a tweet.
+     *
+     * @param md    The instance of Remarkable.
+     */
     private parseTweets(md) : void {
-        // Render our video in an iframe.
-        md.renderer.rules.video = (tokens, idx, options) => {
-            let src = tokens[idx].src;
-            return '<iframe width="1280" height="720" src="' + src
-                +'?rel=0" frameborder="0" allowfullscreen></iframe>';
+        md.block.ruler.push('tweet', state => {
+            return this.parseMedia(state, '#', 'tweet');
+        });
+
+        // Render our tweet
+        md.renderer.rules.tweet = (tokens, idx, options) => {
+            let link = tokens[idx].link;
+            return '<blockquote class="twitter-tweet" data-lang="en">' +
+                '<a href="' + link + '"></a>' +
+                '</blockquote>' +
+                '<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
         };
     }
 
-    private parseMedia() : void {
+    /**
+     * @internal
+     *
+     * @param state
+     * @param startingMarker
+     * @param tokenName
+     * @returns {boolean}
+     */
+    private parseMedia(state, startingMarker, tokenName) : boolean {
+        // Store local copy of position so we don't overwrite it if we need to revert.
+        let pos = state.pos;
+        let marker = state.src.charCodeAt(pos++);
 
+        // Expect a series of tokens that matches the substring "[](" (prefixed by a
+        // starting marker, `!` for images, `@` for videos, `#` for tweets).
+        // This defines the beginning of our pattern.
+        if (marker !== String.fromCharCode(startingMarker)) { return false; }
+        marker = state.src.charCodeAt(pos++);
+        if (marker !== 0x5B /* [ */) { return false; }
+        marker = state.src.charCodeAt(pos++);
+        if (marker !== 0x5D /* ] */) { return false; }
+        marker = state.src.charCodeAt(pos++);
+        if (marker !== 0x28 /* ( */) { return false; }
+
+        // Begin URL. While the current position is less than the length of the string,
+        // increment until we find a closing tag ")". Everything in between is the URL.
+        let urlStart = pos;
+        while (pos < state.posMax) {
+            marker = state.src.charCodeAt(pos++);
+
+            if (marker === 0x29 /* ) */) {
+                break;
+            }
+        }
+
+        // Generate our token
+        let token = {
+            type: tokenName,
+            level: state.level,
+            link: state.src.slice(urlStart, pos - 1),
+            content: marker
+        };
+
+        // Update the state position as the token successfully completed, push the token
+        // onto the tokens array, and return true.
+        state.pos = pos;
+        state.push(token);
+        return true;
+    }
+
+    private parseCaption() :void {
+
+    }
+
+    private parseStringLiteral() : void {
+        
     }
 }
